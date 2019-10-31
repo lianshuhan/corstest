1c21d3d66574ff2adb17fe02bb560c242b01e1ca
admin <admi@qq.com>
2019-10-31 09:23:53 +0800

    test

--- src/main/java/cn/wzvtc/soft/CorsTestApplication.java
+++ src/main/java/cn/wzvtc/soft/CorsTestApplication.java
@@ -3,7 +3,10 @@ package cn.wzvtc.soft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
+//允许跨域访问
 @SpringBootApplication
 public class CorsTestApplication {
     public static void main(String[] args) {
